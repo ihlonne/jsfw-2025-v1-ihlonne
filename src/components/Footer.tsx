10e80137f6629f31ac1bb74e6930b9cc434a187a
Ingelinn Helene <ingelinn92@gmail.com>
2025-04-28 15:10:13 +0200

    create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className='bg-gray-950 text-gray-100 width-full flex flex-col md:flex-row justify-center items-center py-8 m px-18 mt-16'>
+      <div>
+        <p>
+          Created by{' '}
+          <a
+            href='https://github.com/ihlonne'
+            target='_blank'
+          >
+            @ihlonne
+          </a>
+          🌸
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
