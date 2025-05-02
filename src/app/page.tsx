2c77cb8a9a45edd000a9018b29dc2423dbece96e
Ingelinn Helene <ingelinn92@gmail.com>
2025-05-02 14:14:23 +0200

    link to product via id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import getProducts from '@/lib/productApi';
+import { getProducts } from '@/lib/productApi';
 import { useEffect, useState } from 'react';
 import { Product } from '@/types';
 import ProductCard from '@/components/ProductCard';
+import Link from 'next/link';
 
 export default function Home() {
   const [products, setProducts] = useState<
@@ -21,10 +22,15 @@ export default function Home() {
     <div className='flex align-center justify-center m-[0 auto] w-full'>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 max-w-[1290px]'>
         {products.map((product) => (
-          <ProductCard
-            product={product}
+          <Link
+            href={`/product/${product.id}`}
             key={product.id}
-          />
+          >
+            <ProductCard
+              product={product}
+              key={product.id}
+            />
+          </Link>
         ))}
       </div>
     </div>
