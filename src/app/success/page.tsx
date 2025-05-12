13107f1bf5625f2cb304bc687d4dfaad689fd765
Ingelinn Helene <ingelinn92@gmail.com>
2025-05-12 14:49:03 +0200

    create success page

---
+++ src/app/success/page.tsx
@@ -0,0 +1,14 @@
+const SuccessPage = () => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full my-12 h-auto'>
+      <h1 className='text-3xl font-bold'>
+        Your purchase was made successfully!
+      </h1>
+      <p className='mt-12'>
+        Please check your e-mail for confirmation.
+      </p>
+    </div>
+  );
+};
+
+export default SuccessPage;
